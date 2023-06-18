ffd8949f44e427b9419b3a606b3530cfa37f7d51
Emmulatordon <emmulatordon@gmail.com>
2023-06-18 15:23:29 +0100

    Added point of interestDto and mordified the CitiesDataStore

--- CitiesInfo.API/Models/CitiesDataStore.cs
+++ CitiesInfo.API/Models/CitiesDataStore.cs
@@ -8,10 +8,75 @@
         {
             Cities = new List<CityDto>()
             {
-                new CityDto(){Id= 1, Name="Bamenda", Description = "Homeland"},
-                new CityDto(){Id= 2, Name="Buea", Description = "South WEst"},
-                new CityDto(){Id= 3, Name="Lime", Description = "the land of nice beach"},
-                new CityDto(){Id= 4, Name="Douala", Description = "Economic capital of Cameroon"}
+                new CityDto()
+                {
+                    Id= 1, 
+                    Name="Bamenda", 
+                    Description = "Homeland",
+                    PointsOfInterest= new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id= 1,
+                            Name="Nkwen",
+                            Description = "The main street"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id= 2,
+                            Name="Up Station",
+                            Description = "The yaounde of bamenda"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id= 3,
+                            Name="Bambili",
+                            Description = "The University of Bambili"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id= 2,
+                    Name="Buea",
+                    Description = "South West",
+                    PointsOfInterest= new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id= 1,
+                            Name="Moya",
+                            Description = "The main street in Buea"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id= 2,
+                            Name="Up Station of Buea",
+                            Description = "The yaounde of Buea"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id= 3,
+                    Name="Douala",
+                    Description = "The economic capital of Cameroon",
+                    PointsOfInterest= new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id= 1,
+                            Name="Bonaberi",
+                            Description = "The main street in Douala"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id= 2,
+                            Name="Up Station of Douala",
+                            Description = "The yaounde of Douala"
+                        }
+                    }
+                }
             };
             
         }
